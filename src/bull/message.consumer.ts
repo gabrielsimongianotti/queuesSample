92852a5bd61f918078e1898bb25386343e53f93c
gabrielsimongianotti <gabrielgianotti5@gmail.com>
2022-06-28 01:23:02 -0300

    refactor: romove rabbitmq

--- src/bull/message.consumer.ts
+++ src/bull/message.consumer.ts
@@ -1,19 +1,10 @@
-import { Process, Processor } from '@nestjs/bull';
-import { Job } from 'bull';
-import RabbitmqServer from 'src/rabbitmq/rabbitmq-server';
+import { Process, Processor } from "@nestjs/bull";
+import { Job } from "bull";
 
-@Processor('message-queue')
+@Processor("message-queue")
 export class MessageConsumer {
-  @Process('message')
+  @Process("message")
   async messageJob(job: Job): Promise<void> {
     console.log(job.data);
-    const server = new RabbitmqServer('amqp://admin:admin@rabbitmq:5672');
-    await server.start();
-    await server.publishInQueue('express', JSON.stringify(job.data));
-    await server.publishInExchange(
-      'amq.direct',
-      'rota2',
-      JSON.stringify(job.data),
-    );
   }
 }
